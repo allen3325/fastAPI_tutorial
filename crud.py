7c696aef44499bc55957b961d0191d6a0eed37a3
allen3325 <00857146@email.ntou.edu.tw>
2023-10-03 13:43:41 +0800

    lesson: update.

--- crud.py
+++ crud.py
@@ -9,6 +9,9 @@ def get_user(db: Session, user_id: int):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+# no ORM -> query -> SELECT * FROM user WHERE user.email == email -> fastAPI <-> db
+# email = drop table -> SQL Injection
+# ORM -> Object -> 操作都用 物件 fastAPI <-> ORM <-> db
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
@@ -18,6 +21,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    # allen = User(email,password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -30,6 +34,7 @@ def get_items(db: Session, skip: int = 0, limit: int = 100):
 
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db_item = models.Item(**item.dict(), owner_id=user_id)
+    # basketball = Item(data, 1)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
--- main.py
+++ main.py
@@ -56,6 +56,8 @@ def get_db():
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
+    # user['email'] -> dict -> normal
+    # user.email -> instance -> OOP 物件導向 -> ORM
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
@@ -86,3 +88,5 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+# PUT DELETE
